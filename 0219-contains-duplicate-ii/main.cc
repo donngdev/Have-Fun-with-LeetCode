5a085d7071cf22b7e94f34493fea56c2220abbb1
donng <donngdev@163.com>
2020-02-15 16:12:27 +0800

    add 0219

---
+++ 0219-contains-duplicate-ii/main.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+  // Time complexity: O(n)
+  // Space complexity: o(n)
+  bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    unordered_map<int, int> record;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+      auto it = record.find(nums[i]); // hash table time complexity is O(1).
+      if (it == record.end())
+        record.insert(pair<int, int>(nums[i], i));
+      else
+      {
+        if (i - it->second <= k)
+          return true;
+        else
+          it->second = i;  // update the value of nums[i].
+      }
+    }
+    return false;
+  }
+};
+
+int main()
+{
+  vector<int> nums = {1, 0, 1, 1};
+  int k = 1;
+  cout << Solution().containsNearbyDuplicate(nums, k) << endl;
+  return 0;
+}
