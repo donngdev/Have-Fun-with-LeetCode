8275bc2d314ed187f3a1481601f75da9edeaa04c
donng <donngdev@163.com>
2020-02-24 13:11:04 +0800

    add 0404

---
+++ 0404-sum-of-left-leaves/main.cc
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Recursion
+    // Time complexity: O(n)
+    // Space complexity: O(logn)
+    int sumOfLeftLeaves(TreeNode* root)
+    {
+      if (root == NULL)
+        return 0;
+      if (root->left && root->left->left == NULL && root->left->right == NULL)
+        return root->left->val + sumOfLeftLeaves(root->right);
+      else
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
