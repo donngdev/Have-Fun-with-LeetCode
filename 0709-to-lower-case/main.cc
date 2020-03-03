f81624d0ae6da97caa5a8cb516097adbf9387077
donng <donngdev@163.com>
2020-03-03 15:47:29 +0800

    add 0709

---
+++ 0709-to-lower-case/main.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    string toLowerCase(string str)
+    {
+      for (int i = 0; i < str.size(); ++i)
+      {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+          str[i] = str[i] + 32;
+      }
+      return str;
+    }
+};
