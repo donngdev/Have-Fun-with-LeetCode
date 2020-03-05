4c8e25c71632c599e45d2cad1150940f0e4650f4
donng <donngdev@163.com>
2020-03-05 10:44:52 +0800

    add 0024-fan-zhuan

---
+++ 0024-fan-zhuan-lian-biao-lcof/main.cc
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    ListNode* reverseList(ListNode* head)
+    {
+      ListNode* prev = NULL;
+      ListNode* curr = head;
+      while (curr)
+      {
+        // reverse
+        ListNode* next_node = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next_node;
+      }
+
+      return prev;
+    }
+};
