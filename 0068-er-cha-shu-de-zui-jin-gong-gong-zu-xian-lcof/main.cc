bc276e18ee36da49f735366e3c64cf2d1efdf84d
donng <donngdev@163.com>
2020-03-05 11:31:51 +0800

    add 0068

---
+++ 0068-er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/main.cc
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Time complexity: O(n)
+    // Space complexity: O(1)  // recursion stack O(logn)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (!root || root == p || root == q)
+          return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right)
+          return root;
+        
+        return left != NULL ? left : right;
+    }
+};
